3dff7f98bbf09d0d2be640b4b2fc0d25f3151f6f
pinzolo <pinzolo@gmail.com>
2014-05-23 15:48:07 +0900

    Update deploy.rb
    If target file is symlink of source file, not update target file

--- deploy.rb
+++ deploy.rb
@@ -1,28 +1,45 @@
 #!/usr/bin/env ruby
 
+def deletable?(dest, src)
+  if File.symlink?(dest)
+    File.expand_path(File.readlink(dest)) != src
+  else
+    File.exists?(dest)
+  end
+end
+
 files = %w(bashrc gemrc gitattributes gitconfig gitignore gvimrc railsrc rspec screenrc vimrc zshenv zshrc)
 dirs = %w(vim)
 
 home = File.expand_path('~')
 base = File.expand_path(File.dirname(__FILE__))
-puts base
+
 files.each do |file|
   src = "#{base}/#{file}"
   dest = "#{home}/.#{file}"
-  if File.exists?(dest) || File.symlink?(dest)
+  if deletable?(dest, src)
     File.delete(dest)
     puts "deleted #{dest}"
   end
-  File.symlink("#{src}", "#{dest}")
-  puts "created #{dest} -> #{src}"
+  if File.exists?(dest)
+    puts "#{dest} is valid"
+  else
+    File.symlink("#{src}", "#{dest}")
+    puts "created #{dest} -> #{src}"
+  end
 end
+
 dirs.each do |dir|
   src = "#{base}/#{dir}"
   dest = "#{home}/.#{dir}"
-  if File.exists?(dest) || File.symlink?(dest)
+  if deletable?(dest, src)
     File.delete(dest)
     puts "deleted #{dest}"
   end
-  File.symlink("#{src}", "#{dest}")
-  puts "created #{dest} -> #{src}"
+  if File.exists?(dest)
+    puts "#{dest} is valid"
+  else
+    File.symlink("#{src}", "#{dest}")
+    puts "created #{dest} -> #{src}"
+  end
 end
