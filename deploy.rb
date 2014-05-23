915531f89c8ab7db3745d540442663b81686fb7e
pinzolo <pinzolo@gmail.com>
2014-05-23 15:22:28 +0900

    Rmove dot from env files and dirs

---
+++ deploy.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+files = %w(bashrc gemrc gitconfig gitignore gvimrc railsrc rspec screenrc vimrc zshenv zshrc)
+dirs = %w(vim)
+
+home = File.expand_path('~')
+base = File.expand_path(File.dirname(__FILE__))
+puts base
+files.each do |file|
+  src = "#{base}/#{file}"
+  dest = "#{home}/.#{file}"
+  if File.exists?(dest) || File.symlink?(dest)
+    File.delete(dest)
+    puts "deleted #{dest}"
+  end
+  File.symlink("#{src}", "#{dest}")
+  puts "created #{dest} -> #{src}"
+end
+dirs.each do |dir|
+  src = "#{base}/#{dir}"
+  dest = "#{home}/.#{dir}"
+  if File.exists?(dest) || File.symlink?(dest)
+    File.delete(dest)
+    puts "deleted #{dest}"
+  end
+  File.symlink("#{src}", "#{dest}")
+  puts "created #{dest} -> #{src}"
+end
