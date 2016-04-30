452952e19e7f467e1966e207125830d7f3eab3f0
pinzolo <pinzolo@gmail.com>
2016-05-01 01:58:54 +0900

    Add tigrc

--- deploy.rb
+++ deploy.rb
@@ -8,7 +8,7 @@ def deletable?(dest, src)
   end
 end
 
-files = %w(bashrc gemrc gitattributes gitconfig gitignore gvimrc pryrc railsrc rspec screenrc tmux.conf vimrc zshenv zshrc)
+files = %w(bashrc gemrc gitattributes gitconfig gitignore gvimrc pryrc railsrc rspec screenrc tigrc tmux.conf vimrc zshenv zshrc)
 dirs = %w(vim)
 
 home = File.expand_path('~')
